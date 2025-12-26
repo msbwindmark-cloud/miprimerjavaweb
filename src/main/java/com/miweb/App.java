62898bae8f27fd66975d43574c2019229e67ec69
msbwindmark-cloud <msb.windmark@gmail.com>
2025-12-26 16:01:39 +0100

    Create App.java

---
+++ src/main/java/com/miweb/App.java
@@ -0,0 +1,13 @@
+package com.miweb;
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        // Koyeb nos da el puerto en una variable de entorno
+        String portStr = System.getenv("PORT");
+        int port = (portStr != null) ? Integer.parseInt(portStr) : 8080;
+        port(port);
+
+        get("/", (req, res) -> "¡Hola! Mi aplicación Java ya funciona en Koyeb 🚀");
+    }
+}
